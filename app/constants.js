cf3c767c03a5e52e641276ae961c6852cb4df3bd
rylienellis <57364564+rylienellis@users.noreply.github.com>
2020-06-04 21:01:32 -0400

    Create constants.js

---
+++ app/constants.js
@@ -0,0 +1,8 @@
+
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.years = ["2016", "2017", "2018", "2019"];
+    exports.dummies = ["1"];
+});
+//# sourceMappingURL=constants.js.map
